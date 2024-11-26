92de5fb053b07d4dac5f2b43d43f52912dcabe6c
eto8201 <namjun88@gmail.com>
2024-11-27 00:21:20 +0900

    flask 2

--- flask_main.py
+++ flask_main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from extractors.indeed import
 
 app = Flask("JobScraper")
 
@@ -7,8 +8,10 @@ def home():
     return render_template("home.html", name="namjun")
 
 @app.route("/search")
-def home():
-    return render_template("home.html", name="namjun")
+def search():
+    keyword = request.args.get("keyword")
+    print(request.args)
+    return render_template("search.html", name="namjun", keyword=keyword)
 
 
 app.run("0.0.0.0", port="8999")
