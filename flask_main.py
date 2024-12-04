4792058a03421d4fbd9b15bc1d240a4d19932c0b
eto8201 <namjun88@gmail.com>
2024-12-04 23:52:32 +0900

    flask 3

--- flask_main.py
+++ flask_main.py
@@ -3,6 +3,9 @@ from extract_job import extract_job
 
 app = Flask("JobScraper")
 
+db = {
+}
+
 @app.route("/")
 def home():
     return render_template("home.html", name="namjun")
@@ -10,8 +13,12 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    jobs = extract_job(keyword)
-    print(request.args)
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        jobs = extract_job(keyword)
+        print(request.args)
+        db[keyword] = jobs
     return render_template("search.html", name="namjun", keyword=keyword, jobs=jobs)
 
 
